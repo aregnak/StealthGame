c19d7479992c20f9314a592eb559dcc29ea81066
areg <areg.nakashian@gmail.com>
2025-08-02 14:14:14 -0400

    Add acceleration to player movement
    Signed-off-by: areg <areg.nakashian@gmail.com>

--- src/playerController.cpp
+++ src/playerController.cpp
@@ -1,4 +1,5 @@
 #include "playerController.h"
+#include "godot_cpp/variant/vector2.hpp"
 #include "godot_cpp/variant/vector3.hpp"
 #include <godot_cpp/variant/transform3d.hpp>
 #include <godot_cpp/classes/input.hpp>
@@ -42,15 +43,21 @@ void PlayerController::_physics_process(double delta)
     godot::Vector3 direction =
         (get_transform().basis.xform(godot::Vector3(input_dir.x, 0, input_dir.y)).normalized());
 
+    godot::Vector2 vel2d(velocity.x, velocity.z);
+
     if (direction != godot::Vector3())
     {
-        velocity.x = direction.x * speed;
-        velocity.z = direction.z * speed;
+        vel2d.x += direction.x * speed;
+        vel2d.y += direction.z * speed;
+        vel2d = vel2d.limit_length(speed);
+
+        velocity.x = vel2d.x;
+        velocity.z = vel2d.y;
     }
     else
     {
-        velocity.x = godot::Math::move_toward(velocity.z, 0, speed);
-        velocity.z = godot::Math::move_toward(velocity.z, 0, speed);
+        velocity.x = godot::Math::move_toward(velocity.x, 0, float(speed * delta));
+        velocity.z = godot::Math::move_toward(velocity.z, 0, float(speed * delta));
     }
 
     //velocity = velocity.normalized();

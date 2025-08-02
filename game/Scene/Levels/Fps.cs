367fa66f3940072dc1d33c763b87bb31490fe35b
areg <areg.nakashian@gmail.com>
2025-08-02 00:17:46 -0400

    Change fps counter color
    Signed-off-by: areg <areg.nakashian@gmail.com>

--- game/Scene/Levels/Fps.cs
+++ game/Scene/Levels/Fps.cs
@@ -6,6 +6,8 @@ public partial class Fps : Label
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		// Black fps counter for now
+		AddThemeColorOverride("font_color", new Color(0f, 0f, 0f));
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.

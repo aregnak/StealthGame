b3d1e0bf92e99de144d67bac88b65c3b113800e2
areg <areg.nakashian@gmail.com>
2025-08-13 22:38:25 -0400

    working animations
    Signed-off-by: areg <areg.nakashian@gmail.com>

--- src/playerSkin.cpp
+++ src/playerSkin.cpp
@@ -9,23 +9,23 @@ PlayerSkin::PlayerSkin()
 
 void PlayerSkin::_ready()
 {
-    // anim_player = get_node<godot::AnimationPlayer>("AnimationPlayer");
+    anim_player = get_node<godot::AnimationPlayer>("AnimationPlayer");
 
-    // anim_tree = get_node<godot::AnimationTree>("PlayerSkin/AnimationTree");
-    // anim_tree->set_active(true);
-    // move_state_machine = anim_tree->get("parameters/MoveStateMachine/playback");
-    // attack_one_shot = anim_tree->get("parameters/AttackOneShot");
+    anim_tree = get_node<godot::AnimationTree>("AnimationTree");
+    anim_tree->set_active(true);
+    move_state_machine = anim_tree->get("parameters/MoveStateMachine/playback");
+    attack_one_shot = anim_tree->get("parameters/AttackOneShot");
 }
 
 void PlayerSkin::play_attack_anim()
 {
-    // anim_tree->set("parameters/AttackOneShot/request",
-    //                (int)godot::AnimationNodeOneShot::ONE_SHOT_REQUEST_FIRE);
+    anim_tree->set("parameters/AttackOneShot/request",
+                   (int)godot::AnimationNodeOneShot::ONE_SHOT_REQUEST_FIRE);
 }
 
 void PlayerSkin::set_move_state(godot::StringName state)
 {
-    // move_state_machine->travel(state); //
+    move_state_machine->travel(state); //
 }
 
 void PlayerSkin::_bind_methods() {}

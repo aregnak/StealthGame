0f1512a6d8b568a05036073d38af61118085455c
areg <areg.nakashian@gmail.com>
2025-08-15 19:37:35 -0400

    add running animation
    Signed-off-by: areg <areg.nakashian@gmail.com>

--- src/enemyController.cpp
+++ src/enemyController.cpp
@@ -14,8 +14,15 @@ EnemyController::EnemyController()
 
 void EnemyController::_ready()
 {
+    skin = get_node<godot::Node3D>("Skin");
+    anim_player = get_node<godot::AnimationPlayer>("Skin/AnimationPlayer");
+    anim_tree = get_node<godot::AnimationTree>("Skin/AnimationTree");
+    anim_tree->set_active(true);
+    move_state_machine = anim_tree->get("parameters/MoveStateMachine/playback");
+
     enemy_skin = get_node<godot::Node3D>("Skin");
     ray = get_node<godot::RayCast3D>("Ray");
+    turn_timer = get_node<godot::Timer>("TurnTimer");
 }
 
 void EnemyController::_physics_process(double delta)
@@ -34,10 +41,21 @@ void EnemyController::_physics_process(double delta)
     }
 
     static double target_yaw = 0;
-    if (ray->is_colliding())
+    if (ray->is_colliding()) // && turn_timer->is_stopped())
     {
+        // turn_timer->start();
+        // if (turn_timer->get_time_left() > 0.5)
+        // {
+        //     velocity.x = 0;
+        //     velocity.z = 0;
+        //     set_velocity(velocity);
+        //     move_and_slide();
+        // }
+        // else
+        // {
         godot::Vector3 rotation = enemy_skin->get_rotation();
         target_yaw = rotation.y + Math_PI; // flip direction
+        // }
     }
 
     godot::Basis basis = enemy_skin->get_global_transform().basis;
@@ -58,6 +76,15 @@ void EnemyController::_physics_process(double delta)
 
     set_velocity(velocity);
     move_and_slide();
+
+    if (direction != godot::Vector3())
+    {
+        move_state_machine->travel("Running");
+    }
+    else
+    {
+        move_state_machine->travel("idle");
+    }
 }
 
 void EnemyController::_bind_methods() {}
--- src/enemyController.h
+++ src/enemyController.h
@@ -3,15 +3,24 @@
 #include <godot_cpp/classes/character_body3d.hpp>
 #include <godot_cpp/classes/node3d.hpp>
 #include <godot_cpp/classes/ray_cast3d.hpp>
+#include <godot_cpp/classes/animation_player.hpp>
+#include <godot_cpp/classes/animation_tree.hpp>
+#include <godot_cpp/classes/animation_node_state_machine_playback.hpp>
+#include <godot_cpp/classes/animation_node_one_shot.hpp>
+#include <godot_cpp/classes/timer.hpp>
 
 class EnemyController : public godot::CharacterBody3D
 {
     GDCLASS(EnemyController, godot::CharacterBody3D);
 
+    godot::Node3D* skin = nullptr;
+    godot::AnimationPlayer* anim_player = nullptr;
+    godot::AnimationTree* anim_tree = nullptr;
+    godot::Ref<godot::AnimationNodeStateMachinePlayback> move_state_machine;
+
     godot::Node3D* enemy_skin = nullptr;
     godot::RayCast3D* ray = nullptr;
-    // Random direction for now
-    // Will change to adapt to environment
+    godot::Timer* turn_timer = nullptr;
 
     const float speed;
 
